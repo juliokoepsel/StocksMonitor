19b9bcc28a7d65ef70bb659e8874ff08e903244a
JulioKoepsel <julio.koepsel@gmail.com>
2024-06-18 02:17:32 -0300

    Finished developing client and server

--- app/src/main/java/stocksmonitor/App.java
+++ app/src/main/java/stocksmonitor/App.java
@@ -12,16 +12,19 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -39,24 +42,29 @@ import javax.swing.table.JTableHeader;
 import javax.swing.table.TableRowSorter;
 
 public class App extends JFrame {
-    private static final String SERVER_ADDRESS = "localhost";// Define address in a text file
-    private static final int SERVER_PORT = 12345;// Define port in a text file
-    private DefaultTableModel tableModel;
+    private static DefaultTableModel tableModel;
+
     private JTable itemList;
     private JButton addButton;
     private JButton removeButton;
     private JTextField textField;
-    private Set<String> existingElements;
     private Map<Integer, SortOrder> columnSortOrderMap;
 
+    private static Set<String> availableElements;
+    private static List<String> existingElements;
+    private static List<Float> existingValues;
+
+    private static String SERVER_ADDRESS = "localhost";
+    private static int SERVER_PORT = 31415;
+
     public App() {
-        setTitle("StocksMonitor");
+        setTitle("📈  StocksMonitor");
         setSize(450, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setMinimumSize(new Dimension(450, 300));
 
-        tableModel = new DefaultTableModel(new String[]{"Nome 📈", "Valor 📉"}, 0);
+        tableModel = new DefaultTableModel(new String[] { "Nome", "Valor (BRL)" }, 0);
         itemList = new JTable(tableModel);
 
         TableRowSorter<DefaultTableModel> sorter = new TableRowSorter<>(tableModel);
@@ -72,15 +80,20 @@ public class App extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String newItem = textField.getText().trim();
-                if (!newItem.isEmpty() && !existingElements.contains(newItem)) {
-                    Object[] row = {newItem, "--"};
+                if (!newItem.isEmpty() && !existingElements.contains(newItem) && availableElements.contains(newItem)) {
+                    Object[] row = { newItem, "0.0" };
                     tableModel.addRow(row);
                     existingElements.add(newItem);
+                    existingValues.add(0.0f);
                     textField.setText("");
                     saveToFile();
                     System.out.println("Inserção conluída!");
                 } else if (existingElements.contains(newItem)) {
-                    JOptionPane.showMessageDialog(App.this, "Elemento já existe na lista!", "Erro de inserção", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(App.this, "Elemento já existe na lista!", "Erro de inserção",
+                            JOptionPane.ERROR_MESSAGE);
+                } else if (!availableElements.contains(newItem)) {
+                    JOptionPane.showMessageDialog(App.this, "Elemento não foi encontrado!", "Erro de inserção",
+                            JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
@@ -92,9 +105,14 @@ public class App extends JFrame {
                 int selectedViewRow = itemList.getSelectedRow();
                 if (selectedViewRow != -1) {
                     int selectedModelRow = itemList.convertRowIndexToModel(selectedViewRow);
-                    String removedElement = tableModel.getValueAt(selectedModelRow, 0).toString();
+                    String selectedValue = tableModel.getValueAt(selectedModelRow, 0).toString();
                     tableModel.removeRow(selectedModelRow);
-                    existingElements.remove(removedElement);
+                    for (int i = 0; i < existingElements.size(); i++) {
+                        if (existingElements.get(i).equals(selectedValue)) {
+                            existingElements.remove(i);
+                            existingValues.remove(i);
+                        }
+                    }
                     saveToFile();
                     System.out.println("Remoção conluída!");
                 }
@@ -114,7 +132,6 @@ public class App extends JFrame {
         getContentPane().add(inputPanel, BorderLayout.NORTH);
         getContentPane().add(scrollPane, BorderLayout.CENTER);
 
-        existingElements = new HashSet<>();
         columnSortOrderMap = new HashMap<>();
         for (int i = 0; i < tableModel.getColumnCount(); i++) {
             columnSortOrderMap.put(i, SortOrder.UNSORTED);
@@ -131,14 +148,15 @@ public class App extends JFrame {
                     if (nextSortOrder == SortOrder.UNSORTED) {
                         sorter.setSortKeys(null);
                     } else {
-                        sorter.setSortKeys(Collections.singletonList(new RowSorter.SortKey(columnIndex, nextSortOrder)));
+                        sorter.setSortKeys(
+                                Collections.singletonList(new RowSorter.SortKey(columnIndex, nextSortOrder)));
                     }
                     columnSortOrderMap.put(columnIndex, nextSortOrder);
                 }
             }
         });
 
-        populateTableFromFile();
+        populateElements();
     }
 
     private SortOrder getNextSortOrder(SortOrder currentSortOrder) {
@@ -152,86 +170,147 @@ public class App extends JFrame {
         }
     }
 
-    private void populateTableFromFile() {
-        File file = new File("list.txt");
+    private static void saveToFile() {
+        String fileName = "list.txt";
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            for (int i = 0; i < existingElements.size(); i++) {
+                writer.write(existingElements.get(i));
+                writer.write(",");
+                writer.write(existingValues.get(i).toString());
+                writer.write("\n");
+            }
+            writer.close();
+            System.out.println("(" + fileName + "): Gravação concluída!");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Erro de gravação: Erro ao gravar " + fileName);
+        }
+    }
+
+    private static void readConnectionFile() {
+        String fileName = "client_connection.cfg";
+        File file = new File(fileName);
+        if (file.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+                String line = br.readLine();
+                if (line != null) {
+                    SERVER_ADDRESS = line;
+                }
+                line = br.readLine();
+                if (line != null) {
+                    SERVER_PORT = Integer.parseInt(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Erro de leitura: Erro ao ler " + fileName);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                System.err.println("Erro de formatação numérica: Erro ao ler " + fileName);
+            }
+        } else {
+            try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+                bw.write(SERVER_ADDRESS);
+                bw.newLine();
+                bw.write(Integer.toString(SERVER_PORT));
+                System.out.println("(" + fileName + "): Gravação concluída!");
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.err.println("Erro de gravação: Erro ao gravar " + fileName);
+            }
+        }
+    }
+
+    private static void populateAvailableElements() {
+        availableElements = new HashSet<>();
+        availableElements.add("MGLU3");
+        availableElements.add("PETR4");
+        availableElements.add("RRRP3");
+        availableElements.add("GOLD11");
+        availableElements.add("COGN3");
+        availableElements.add("ABEV3");
+        availableElements.add("VALE3");
+        availableElements.add("SOJA3");
+        availableElements.add("EMBR3");
+        availableElements.add("OIBR4");
+    }
+
+    private static void populateElements() {
+        existingElements = new ArrayList<>();
+        existingValues = new ArrayList<>();
+        String fileName = "list.txt";
+        File file = new File(fileName);
         if (file.exists()) {
             try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 String line;
                 while ((line = reader.readLine()) != null) {
                     String[] columns = line.split(",");
                     if (columns.length == 2) {
-                        tableModel.addRow(columns);
                         existingElements.add(columns[0]);
+                        existingValues.add(Float.parseFloat(columns[1]));
                     }
                 }
-                System.out.println("Leitura concluída!");
+                System.out.println("(" + fileName + "): Leitura concluída!");
             } catch (IOException e) {
                 e.printStackTrace();
-                JOptionPane.showMessageDialog(this, "Erro ao ler a lista.", "Erro de leitura", JOptionPane.ERROR_MESSAGE);
+                System.err.println("Erro de leitura: Erro ao ler " + fileName);
             }
+        }
+        for (int i = 0; i < existingElements.size(); i++) {
+            String[] aux = new String[2];
+            aux[0] = existingElements.get(i);
+            aux[1] = existingValues.get(i).toString();
+            tableModel.addRow(aux);
         }
     }
 
-    private void saveToFile() {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("list.txt"))) {
-            for (int i = 0; i < tableModel.getRowCount(); i++) {
-                for (int j = 0; j < tableModel.getColumnCount(); j++) {
-                    writer.write(tableModel.getValueAt(i, j).toString());
-                    if (j < tableModel.getColumnCount() - 1) {
-                        writer.write(",");
-                    }
-                }
-                writer.write("\n");
-            }
-            System.out.println("Gravação concluída!");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Erro ao gravar a lista.", "Erro de gravação", JOptionPane.ERROR_MESSAGE);
-        }
+    private void refreshTable() {
+        tableModel.setRowCount(0);
+        existingElements.clear();
+        populateElements();
     }
 
     public static void main(String[] args) {
+        readConnectionFile();
+        populateAvailableElements();
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
                 App listGUI = new App();
                 listGUI.setVisible(true);
+                System.out.println("Servidor: " + SERVER_ADDRESS + ":" + SERVER_PORT);
+
+                Timer timer = new Timer();
+                TimerTask task = new TimerTask() {
+                    @Override
+                    public void run() {
+                        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
+                                ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+                                ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
+                            System.out.println("Realizando requisição ao servidor!");
+                            out.writeObject(existingElements);
+                            out.flush();
+                            System.out.println("Elementos enviados!");
+                            @SuppressWarnings("unchecked")
+                            List<Float> receivedElements = (List<Float>) in.readObject();
+                            System.out.println("Resposta:");
+                            for (Float element : receivedElements) {
+                                System.out.println(element);
+                            }
+                            existingValues = receivedElements;
+                            saveToFile();
+                            listGUI.refreshTable();
+                        } catch (IOException | ClassNotFoundException e) {
+                            e.printStackTrace();
+                            System.err
+                                    .println("Erro de conexão: Erro ao enviar dados ao servidor " + SERVER_ADDRESS + ":"
+                                            + SERVER_PORT);
+                        }
+                    }
+                };
+                timer.schedule(task, 0, 60000);
             }
         });
 
-        // -----------------------------------------------------------------------------------------------
-        /*
-        Timer timer = new Timer();
-
-        Map<String, Float> stockPrices = new HashMap<>();
-
-        int scheduleTimeout = 30000;
-
-        TimerTask task = new TimerTask() {
-        public void run() {
-            System.out.println("Execução do scheduler");
-            StockPriceSearcher searcher = new StockPriceSearcher(existingElements, stockPrices);
-
-            searcher.updateStockPrices();
-        }
-        };
-        timer.scheduleAtFixedRate(task, 0, scheduleTimeout);
-        */
-        // -----------------------------------------------------------------------------------------------
-        try (Socket socket = new Socket(SERVER_ADDRESS, SERVER_PORT);
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
-
-            System.out.println("Conectado ao servidor");
-
-            String userInput;
-            while ((userInput = stdIn.readLine()) != null) {
-                out.println(userInput);
-                System.out.println("Resposta do servidor: " + in.readLine());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
--- app/src/main/java/stocksmonitor/StockPriceSearcher.java
+++ app/src/main/java/stocksmonitor/StockPriceSearcher.java
@@ -1,29 +1,35 @@
 package stocksmonitor;
 
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class StockPriceSearcher {
-    private Set<String> symbols;
+    private List<String> symbols;
     private int maxConcurrentThreads = 2;
     private Map<String, Float> prices;
 
-    public StockPriceSearcher(Set<String> symbols, Map<String, Float> prices) {
+    public StockPriceSearcher(List<String> symbols, Map<String, Float> prices) {
         this.symbols = symbols;
         this.prices = prices;
     }
 
     public void updateStockPrices() {
-    System.out.println("Início da atualização de valores");
-    ExecutorService executorService = Executors.newFixedThreadPool(maxConcurrentThreads);
+        System.out.println("Início da atualização de valores");
+        ExecutorService executorService = Executors.newFixedThreadPool(maxConcurrentThreads);
 
-    for (String symbol : symbols) {
-        Thread t = new Thread(new Symbol(symbol, prices));
-        executorService.execute(t);
+        for (String symbol : symbols) {
+            Thread t = new Thread(new Symbol(symbol, prices));
+            executorService.execute(t);
+        }
+        try {
+            executorService.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Fim da atualização de valores");
     }
-    executorService.shutdown();
-    System.out.println("Fim da atualização de valores");
-  }
+    
 }
